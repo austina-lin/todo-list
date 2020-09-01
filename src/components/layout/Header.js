9cf2bb90839e28d3910e8c452d6c9a30ce0d4969
Austina Lin <austinaclin@gmail.com>
2020-08-31 22:12:58 -0400

    continued foundation with content and header

---
+++ src/components/layout/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {FaPizzaSlice} from 'react-icons/fa';
+
+export const Header = () => {
+  return (
+      <header className="header">
+        <nav>
+          <div className="logo">
+            <img src="/images/logo.png" alt="Todo List"/>
+          </div>
+          <div className="settings">
+            <ul>
+              <li className="settings__add">+</li>
+              <li className="settings__darkmode"><FaPizzaSlice/></li>
+            </ul>
+          </div>
+        </nav>
+      </header>
+  );
+}
